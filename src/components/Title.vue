895505110002458f94465bfe6dd9a0fce6614669
Eliana-Abate <eliana.abate@outlook.it>
2021-09-03 16:57:47 +0200

    changed color

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -14,7 +14,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h1 {
-  color: #2c3e50;
+  color: rgb(255, 51, 0);
 }
   
 </style>
